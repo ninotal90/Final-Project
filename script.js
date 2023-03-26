3929939f73799ad17739efabcbe84426e8a546b3
ninotal90 <ntalakvadze90@gmail.com>
2023-03-26 13:13:28 +0400

    Added final project

---
+++ script.js
@@ -0,0 +1,13 @@
+// burger bar
+let navigation = document.getElementById('navBar');
+let burger = document.getElementById('burgerBar');
+
+burger.addEventListener('click', function() {
+    navigation.classList.toggle('activeNav');
+})
+
+// alert
+let buttonl = document.getElementById('buttonClick')
+buttonClick.addEventListener('click', function(){
+    alert('შესვლა')
+})
